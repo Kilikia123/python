c5352bad50546a562b57fbda2957d6343a69fd9d
ArmKilikia <brsikyan1969@gmail.com>
2021-04-24 02:12:25 +0300

    changes

---
+++ web/test/WeatherTests.py
@@ -0,0 +1,23 @@
+import src.WeatherClass
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def tuneIn(self):
+        self.myWeather = src.WeatherClass.Weather()
+
+    def testGetDegrees(self, ideal_value):
+        my_degrees = self.myWeather.get_degrees()
+        self.assertEqual(my_degrees, ideal_value)
+
+    def testGetProbPrec(self, ideal_value):
+        my_prob_prec = self.myWeather.get_prob_prec()
+        self.assertEqual(my_prob_prec, ideal_value)
+
+    def testGetHumidity(self, ideal_value):
+        my_humidity = self.myWeather.get_humidity()
+        self.assertEqual(my_humidity, ideal_value)
+
+    def testGetWindSpeed(self, ideal_value):
+        my_wind_speed = self.myWeather.get_wind_speed()
+        self.assertEqual(my_wind_speed, ideal_value)
