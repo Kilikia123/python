6424bd3d983c4070acb6d512526032508b9c5fcf
ArmKilikia <brsikyan1969@gmail.com>
2021-04-30 13:30:30 +0300

    chang

--- web/test/weather_tests.py
+++ web/test/weather_tests.py
@@ -3,21 +3,19 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def tuneIn(self):
-        self.myWeather = Weather()
-
     def testGetDegrees(self):
-        my_degrees = int(self.myWeather.get_degrees())
-        self.assertTrue(my_degrees <= 30 and my_degrees >= -30)
+        my_degrees = int(Weather().get_degrees())
+        self.assertTrue(30 >= my_degrees >= -30)
 
     def testGetProbPrec(self):
-        my_prob_prec = int(self.myWeather.get_prob_prec())
-        self.assertTrue(my_prob_prec <= 100 and my_prob_prec >= 0)
+        my_prob_prec = int(Weather().get_prob_prec()[:-1])
+        self.assertTrue(100 >= my_prob_prec >= 0)
 
     def testGetHumidity(self):
-        my_humidity = int(self.myWeather.get_humidity())
-        self.assertTrue(my_humidity <= 100 and my_humidity >= 0)
+        my_humidity = int(Weather().get_humidity()[:-1])
+        self.assertTrue(100 >= my_humidity >= 0)
 
     def testGetWindSpeed(self):
-        my_wind_speed = int(self.myWeather.get_wind_speed())
-        self.assertTrue(my_wind_speed <= 25 and my_wind_speed >= 0)
+        my_wind_speed = int(Weather().get_wind_speed()[:-4])
+        self.assertTrue(25 >= my_wind_speed >= 0)
+
